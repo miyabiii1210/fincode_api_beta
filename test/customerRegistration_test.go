6016269b32506e7f26c9e4f9da6c693b35a2163f
miyabiii1210 <miyabiii1210@gmail.com>
2023-01-21 11:10:47 +0900

    add customer registration api

---
+++ test/customerRegistration_test.go
@@ -0,0 +1,45 @@
+package test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/miyabiii1210/fincode_api_beta/go/pkg/fincode"
+	"github.com/miyabiii1210/fincode_api_beta/go/pkg/util"
+)
+
+func TestCustomerRegistration(t *testing.T) {
+	type args struct {
+		ctx   context.Context
+		order fincode.CustomerRegistrationRequest
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "test format",
+			args: args{
+				ctx: context.TODO(),
+				order: fincode.CustomerRegistrationRequest{
+					ID: "",
+				},
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ret, err := fincode.CustomerRegistration(tt.args.ctx, tt.args.order)
+			if err != nil {
+				t.Errorf("CustomerRegistration error: %v\n", err)
+				return
+			}
+			t.Log("result:", ret)
+			util.Sleep(1)
+			t.Logf("%s fin.\n", tt.name)
+		})
+	}
+}
